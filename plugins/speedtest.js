de857dd024788fccd5e723c7134c82b954520436
AyGemuy <91945119+AyGemuy@users.noreply.github.com>
2022-07-20 18:05:46 +0800

    New
    New

--- plugins/speedtest.js
+++ plugins/speedtest.js
@@ -1,8 +1,10 @@
+//Made By KORONEOFC/SAD-BOT
+
 import cp from 'child_process'
 import { promisify } from 'util'
 let exec = promisify(cp.exec).bind(cp)
-let handler = async (m) => {
-return m.reply('Testing...')
+let handler = async (m, { conn}) => {
+	await conn.reply(m.chat, `Please Wait`, m)
     let o
     try {
         o = await exec('python speed.py')
@@ -10,7 +12,7 @@ return m.reply('Testing...')
         o = e
     } finally {
         let { stdout, stderr } = o
-        if (stdout.trim()) m.reply(stdout)
+        if (stdout.trim()) conn.sendButton(m.chat, `${htki} SPEEDTEST.NET ${htka}`, stdout, null, [["MENU", ".menu"],["PING", ".ping"]], m)
         if (stderr.trim()) m.reply(stderr)
     }
 }
